a3766812e46d2b7e1f0e2c82337b8814de9e19ac
Toni Tralice <atralice@gmail.com>
2016-11-18 19:24:41 -0300

    Modificaciones en clase.

--- materialSoporte/ejercicios/clase5/beatlesTemplate/app.js
+++ materialSoporte/ejercicios/clase5/beatlesTemplate/app.js
@@ -21,12 +21,6 @@ var beatles=[{
   birthdate: "25/02/1946",
   profilePic:"http://az616578.vo.msecnd.net/files/2016/03/09/635931448636931925-692833716_george-harrison-living-in-the-material-world-george-harrison-photo-credit-credit-robert-whitaker-c-apple-corps-ltd-courtesy-of-hbo.jpg",
   wiki: 'https://en.wikipedia.org/wiki/George_Harrison'
-},
-{
-  name: "Richard Starkey",
-  birthdate: "07/08/1940",
-  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg",
-  wiki: 'https://en.wikipedia.org/wiki/Ringo_Starr'
 }
 ]
 
